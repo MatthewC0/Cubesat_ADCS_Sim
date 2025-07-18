Updated KF to EKF with the jacobian of euler rotational equations. Successfully implemented PID controller. Optimized/Updated 3D plot feature and added updating text. Fixed quaternion kinematics issues and added normalization. Updated euler dynamics. Created .py file for LQR controller to be implemented at a later date.

analysis/plot_3d.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pyvista as pv
+import vtk
 from scipy.spatial.transform import Rotation as R
 
 
@@ -9,28 +10,45 @@ def plot_3d(data, time, filename):
     t = time
     pl = pv.Plotter()
 
-    # Displaying body-fixed frame unit vector arrows
+    # Displaying INERTIAL-FIXED frame unit vector arrows
     cent = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
     dir = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
     mag = 2
-    pl.add_arrows(cent=cent, direction=dir, mag=mag)
+    pl.add_arrows(cent=cent, direction=dir, mag=mag, color='black')
     pl.add_axes(line_width=2)
 
     # Adding cubesat object to the scene
     cubesat = pv.Cube(center=(0, 0, 0), x_length=3.0, y_length=2.0, z_length=1.0)
     actor = pl.add_mesh(cubesat, color='r',show_edges=True)
 
+    # Displaying BODY-FIXED frame unit vector arrows
+    body_arrows = pl.add_arrows(cent=cent, direction=dir, mag=mag, color='blue')
+
+    # Text displays
+    q_text = pl.add_text('Quaternion: ', position=(5, 730), font_size=10)
+    t_text = pl.add_text('Time (s): ', position=(5, 690), font_size=10)
+
     # Creating movie file using input and writing initial frame
     pl.open_movie(filename)
     pl.write_frame()
 
     # Looping and writing each frame using quaternion input data
-    for q in quaternion:
+    for i, q in enumerate(quaternion):
         r = R.from_quat(q)
-        rot_mat = np.eye(4)
-        rot_mat[:3, :3] = r.as_matrix()
+        rot_mat = r.as_matrix()
+        mat = np.eye(4)
+        mat[:3, :3] = rot_mat
 
-        actor.user_matrix = rot_mat
+        vtk_matrix = vtk.vtkMatrix4x4()
+        vtk_matrix.DeepCopy(mat.T.flatten(order='F'))
+        body_arrows.SetUserMatrix(vtk_matrix)
+        actor.SetUserMatrix(vtk_matrix)
+
+        q_display = f'Quaternion (x, y, z, w):\n[{q[0]: .3f}, {q[1]: .3f}, {q[2]: .3f}, {q[3]: .3f}]'
+        t_display = f'Time (s):\n{t[i]:.2f}'
+        q_text.SetInput(q_display)
+        t_text.SetInput(t_display)
+
         pl.write_frame()
 
     pl.close()

src/dynamics/attitude_kinematics.py
@@ -3,10 +3,11 @@ from integrators import rk4
 
 
 def qdot(t, q, w):
-    omega = np.array([[0, w[2], -w[1], w[0]],
-                      [-w[2], 0, w[0], w[1]],
-                      [w[1], -w[0], 0, w[2]],
-                      [-w[0], -w[1], -w[2], 0]])
+    wx, wy, wz = w
+    omega = np.array([[0, wz, -wy, wx],
+                      [-wz, 0, wx, wy],
+                      [wy, -wx, 0, wz],
+                      [-wx, -wy, -wz, 0]])
 
     qdot = 0.5*(omega @ q)
     return qdot
@@ -15,7 +16,12 @@ def qdot(t, q, w):
 def quaternion(q, w, dt):
     t = 0.0
     q = rk4(qdot, t, q, dt, w)
-    q /= np.linalg.norm(q)
+    norm = np.linalg.norm(q)
+    if norm > 1e-8:
+        q /= norm
+    else:
+        print("WARNING: Quaternion too small")
+        q = np.array([0.0, 0.0, 0.0, 1.0])
     return q
 
 

src/dynamics/euler_dynamics.py
@@ -3,17 +3,31 @@ from integrators import rk4
 
 
 class EulerDynamics:
+    """Euler dynamics class to simulate euler rotational equations of motion for a given rigid body.
+
+    Args:
+        inertia_matrix (float|[3x3]): Inertia dyadic to define rigid body."""
     def __init__(self, inertia_matrix):
         self.I = inertia_matrix
         self.I_inv = np.linalg.inv(self.I)
 
-    def wdot(self, t, w, external_torque):
+    def jacobian(self, w, torque):
+        wx, wy, wz = w
+        Ix, Iy, Iz = self.I[0,0], self.I[1,1], self.I[2,2]
+        F = np.zeros((3, 3))
+        F[0, 1] = (Iy - Iz) * wz / Ix
+        F[0, 2] = (Iy - Iz) * wy / Ix
+        F[1, 0] = (Iz - Ix) * wz / Iy
+        F[1, 2] = (Iz - Ix) * wx / Iy
+        F[2, 0] = (Ix - Iy) * wy / Iz
+        F[2, 1] = (Ix - Iy) * wx / Iz
+        return F
+
+    def wdot(self, t, w, torque):
         w = np.array(w).reshape(3)
-        external_torque = np.array(external_torque).reshape(3)
+        torque = np.array(torque).reshape(3)
 
-        w_cross_Iomega = np.cross(w, self.I @ w)
-
-        wdot = self.I_inv @ (external_torque - w_cross_Iomega)
+        wdot = self.I_inv @ (torque - np.cross(w, self.I @ w))
         return wdot
 
     def step(self, w, external_torque, dt):

src/estimation/kalman_filter.py
@@ -21,7 +21,7 @@ def low_pass_filter(datapoint, previous_avg, alpha):
 
 
 class kalman_filter():
-    """Kalman filter class for state estimation.
+    """Extended Kalman Filter (EKF) class for state estimation.
 
     Args:
         n (int): Number of states to estimate.
@@ -40,9 +40,13 @@ class kalman_filter():
         self.x = np.zeros(n)
         self.dynamics = dynamics
 
-    def predict(self, torque, dt):
+    def predict(self, torque, dt, jacobian=False):
         self.x = self.dynamics.step(self.x, torque, dt)
-        self.P = self.A @ self.P @ self.A.T + self.Q
+        if jacobian:
+            F = self.dynamics.jacobian(self.x, torque)
+            self.P = F @ self.P @ F.T + self.Q
+        else:
+            self.P = self.A @ self.P @ self.A.T + self.Q
 
     def update(self, w_measured):
         S = self.H @ self.P @ self.H.T + self.R
@@ -51,7 +55,7 @@ class kalman_filter():
         self.x = self.x + K @ y
         self.P = (np.eye(self.n) - K @ self.H) @ self.P
 
-    def step(self, w_measured, torque, dt):
+    def step(self, w_measured, torque, dt, jacobian=False):
         """Predicts the next step and updates the estimated state.
 
         Args:
@@ -59,6 +63,6 @@ class kalman_filter():
             torque (float|[1xn]): External torque values.
             dt (float): Timestep parameter.
         """
-        self.predict(torque, dt)
+        self.predict(torque, dt, jacobian=jacobian)
         self.update(w_measured)
         return self.x

src/main.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import yaml
-from scipy.spatial.transform import Rotation as R
+from scipy.spatial.transform import Rotation as Rot
 from dynamics import EulerDynamics, quaternion, euler_angles
 from analysis import plot, plot_3d
 from sensors.gyro import Gyro
 from estimation.kalman_filter import recursion_average, simple_moving_average, low_pass_filter, kalman_filter
+from control.pid_controller import PIDController, quaternion_error
 
 
 def main():
@@ -14,8 +15,8 @@ def main():
         sat_config = yaml.safe_load(file)
 
     # Setting up simulation time
-    dt = 1  # simulation timestep [sec]
-    tfinal = 1800  # total simulation time [sec]
+    dt = 0.01  # simulation timestep [sec]
+    tfinal = 100  # total simulation time [sec]
     steps = int(tfinal/dt)  # number of simulation steps
     t = np.linspace(0, tfinal, steps+1)  # setting up time matrix
 
@@ -28,22 +29,25 @@ def main():
     q_filtered = np.zeros((steps+1, 4))
     theta_true = np.zeros((steps+1, 3))
     theta_measured = np.zeros((steps+1, 3))
+    theta_filtered = np.zeros((steps+1, 3))
+    external_torque = np.zeros((steps+1, 3))
 
     # Configuring satellite initial conditions and parameters
-    I = sat_config['inertia_tensor']
+    I = np.array(sat_config['inertia_tensor'])
     w_true[0] = np.deg2rad(sat_config['initial_angular_velocity'])
+    # q_true[0] = sat_config['initial_quaternion']
+    # theta_true[0] = A.from_quat(q_true[0]).as_euler('xyz', degrees=False)
     theta_true[0] = np.deg2rad(sat_config['initial_angle'])
     theta_measured[0] = theta_true[0]
-    rot = R.from_euler('xyz', theta_true[0])
+    rot = Rot.from_euler('xyz', theta_true[0])
     q_true[0] = rot.as_quat()
     q_measured[0] = q_true[0]
-    external_torque = np.zeros(3)
 
     # ============================================================================================
     # Simulating gyroscope given Angular Random Walk (ARW), Bias-run, and random measurement noise
-    arw = 0.15  # 0.15  # Angular Rate Walk noise [deg/sqrt(hr)]
-    bias_run = 4.0  # 4.0  # Bias-run noise [deg/hr]
-    measurement_noise = [1.0, 1.0, 1.0]  # general measurement noise [deg/hr]
+    arw = 0.0  # 0.15  # Angular Rate Walk noise [deg/sqrt(hr)]
+    bias_run = 0.0  # 4.0  # Bias-run noise [deg/hr]
+    measurement_noise = [720.0, 720.0, 720.0]  # general measurement noise [deg/hr]
     gyro = Gyro(arw=arw, bias_run=bias_run, measurement_noise=measurement_noise, dt=dt)
     w_measured[0] = gyro.read(w_true[0])
     # ============================================================================================
@@ -53,8 +57,9 @@ def main():
     # ============================================================================================
     # Simulating kalman filter estimation
     w_filtered[0] = w_measured[0]
+    q_filtered[0] = q_measured[0]
     n = 3
-    Q = np.eye(n)*1e-4  # larger Q trusts measurement more while smaller Q trusts model more
+    Q = np.eye(n)*1e-1  # larger Q trusts measurement more while smaller Q trusts model more
     Ra = np.eye(n)*1e-1  # larger R trusts model more while smaller R trust measurement more
     # Testing adding measurement noise from gyroscope, still working on it.
     # Ra_measurement = np.radians(measurement_noise[0])/3600
@@ -62,29 +67,58 @@ def main():
     P = np.eye(n)*1e-0
     kf = kalman_filter(n, Q, Ra, P, euler_dyn)
     # ============================================================================================
-
     # Simple moving average
     # n = 10
     # w_filtered[0] = simple_moving_average(w_measured, 0, n)
-
     # ============================================================================================
     # Low Pass Filter
     # alpha = 0.1
     # w_filtered[0] = low_pass_filter(w_measured[0], 0, alpha)
     # ============================================================================================
 
+    # ============================================================================================
+    # PID controller configuration
+    q_error = np.zeros((steps+1, 3))
+    q_desired = np.array([0.0, 0.0, 0.0, 1.0])
+    w_error = np.zeros((steps+1, 3))
+    w_desired = np.zeros(3)
+    max_torque = 1.5e-3
+    kp = 1e-2
+    ki = 0e-2
+    kd = 3e-2
+    pid = PIDController(kp, ki, kd)
+    # ============================================================================================
+
+    # ============================================================================================
+    # LQR controller configuration
+    Q = np.array([])
+    R = np.array([])
+    # ============================================================================================
+
     for i in range(steps):
-        w_true[i+1] = euler_dyn.step(w_true[i], external_torque, dt)
+        # print('Q TRUE', q_true[i])
+        # print('Q FILTERED', q_filtered[i])
+        # print('Q DESIRED', q_desired)
+        q_error[i] = quaternion_error(q_desired, q_filtered[i])
+        w_error[i] = w_filtered[i]
+        # print('ANGULAR VELOCITY ERROR', w_error[i])
+        external_torque[i] = pid.compute(q_error[i], w_error[i], dt)
+        external_torque[i] = np.clip(external_torque[i], -max_torque, max_torque)
+
+        w_true[i+1] = euler_dyn.step(w_true[i], external_torque[i], dt)
         w_measured[i+1] = gyro.read(w_true[i+1])
         # w_filtered[i+1] = recursion_average(w_measured[i+1], i+1, w_filtered[i])
         # w_filtered[i+1] = simple_moving_average(w_measured, i+1, n)
         # w_filtered[i+1] = low_pass_filter(w_measured[i+1], w_filtered[i], alpha)
-        w_filtered[i+1] = kf.step(w_measured[i+1], external_torque, dt)
+        w_filtered[i+1] = kf.step(w_measured[i+1], external_torque[i+1], dt, jacobian=True)
+
         q_true[i+1] = quaternion(q_true[i], w_true[i], dt)
         q_measured[i+1] = quaternion(q_measured[i], w_measured[i], dt)
         q_filtered[i+1] = quaternion(q_filtered[i], w_filtered[i], dt)
-        theta_true[i+1] = R.from_quat(q_true[i+1]).as_euler('xyz', degrees=False)
-        theta_measured[i+1] = gyro.read(theta_true[i])
+
+        theta_true[i+1] = Rot.from_quat(q_true[i+1]).as_euler('xyz', degrees=False)
+        # theta_measured[i+1] = R.from_quat(q_measured[i+1]).as_euler('xyz', degrees=False)
+        # theta_filtered[i+1] = R.from_quat(q_filtered[i+1]).as_euler('xyz', degrees=False)
 
     # Convert from radians to degrees
     w_true = np.rad2deg(w_true)
@@ -92,6 +126,8 @@ def main():
     w_filtered = np.rad2deg(w_filtered)
     theta_true = np.rad2deg(theta_true)
     theta_measured = np.rad2deg(theta_measured)
+    theta_filtered = np.rad2deg(theta_filtered)
+    w_error = np.rad2deg(w_error)
 
     # TEST/DEBUG
     # print(w_true)
@@ -99,9 +135,14 @@ def main():
     # print(w_filtered)
     # print(q_true)
     # print(q_measured)
+    # print(q_filtered)
     # print(theta_true)
     # print(theta_measured)
+    # print(theta_filtered)
     # print(t)
+    # print('quaternion error', q_error)
+    # print('angular velocity error', w_error)
+    # print('external torque', external_torque)
 
     # MULTI-VARIABLE VISUALIZATIONS
     # w_total = np.hstack((w_measured, w_filtered))
@@ -115,11 +156,39 @@ def main():
     # plot(q_true, t, datatype='quaternion', show_plot=True)
     # plot(q_measured, t, datatype='quaternion', show_plot=True)
     # plot(theta, t, datatype='angle', show_plot=True)
-    # plot_3d(q_true, t, filename)
+    plot_3d(q_true, t, filename)
+
+    w_filter_error = w_true - w_filtered
+    plt.plot(t, w_filter_error[:, 0])
+    plt.legend()
+    plt.grid()
+    plt.show()
 
     plt.plot(t, w_true[:, 0], label='True wx')
     plt.plot(t, w_measured[:, 0], label='Measured wx')
     plt.plot(t, w_filtered[:, 0], label='Filtered wx')
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+    plt.plot(t, w_measured[:, 0], label='True wx')
+    plt.plot(t, w_measured[:, 1], label='True wy')
+    plt.plot(t, w_measured[:, 2], label='True wz')
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+    plt.plot(t, q_measured[:], label='True q')
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+    plt.plot(t, q_error[:], label='Quaternion Error')
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+    plt.plot(t, external_torque, label='Control torque')
     plt.legend()
     plt.grid()
     plt.show()